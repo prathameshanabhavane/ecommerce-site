a83df2937d5768ea745e7eaf7edc2e13de6d221e
prathamesh.a <prathamesh.a@internal.media.net>
2024-09-17 20:31:09 +0530

    product detail page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from "./Pages/Home/Home";
 import Products from "./Pages/Products/Products";
+import ProductDetail from "./Pages/ProductDetail/ProductDetail";
 import Cart from "./Pages/Cart/Cart";
 import Error from "./Components/Error/Error";
 
@@ -11,12 +12,15 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-          <Route path="" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path='*' element={<Error />} />
-      </Routes>
+      <div className="main">
+        <Routes>
+            <Route path="" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path='*' element={<Error />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
