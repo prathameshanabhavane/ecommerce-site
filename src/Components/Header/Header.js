43b6e6abd2a0c18c4852e78120dd98c463f27388
prathamesh.a <prathamesh.a@internal.media.net>
2024-09-29 16:02:56 +0530

    added search component and it's functionality

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,12 +8,19 @@ import { CiShoppingCart } from "react-icons/ci";
 import { Link } from 'react-router-dom';
 import './Header.scss';
 import { useCart } from '../../Context/CartContext';
+import Search from '../Search/Search';
+import { useState } from 'react';
 
 const Header  = () => {
     const {cart, dispatch } = useCart();
+    const [searchToggle, setSearchToggle] = useState(false);
 
-    console.log('Header', cart.length)
+    // console.log('Header', cart.length)
 
+    const handleSearchToggle = () => {
+        console.log('search')
+        setSearchToggle(!searchToggle)
+    }
 
     return(
         <>
@@ -21,7 +28,10 @@ const Header  = () => {
                     <div className='container'>
                         <div className='header-wrapper'>
                             <h1><Link to="/">Dukan</Link></h1>
-                            <div className='menu-wrap'>
+                            <div className={`search-wrapp ${searchToggle ? 'search-active' : ''}`}>
+                                <Search /> 
+                            </div>
+                            <div className="menu-wrap">
                                 <nav>
                                     <ul className='menu-list'>
                                         <li>
@@ -39,6 +49,18 @@ const Header  = () => {
                                     <li>
                                         <CiShoppingCart />
                                         <span className='badge'>{cart.length} </span>
+                                    </li>
+                                    <li className='search-icon-li'>
+                                        <span onClick={handleSearchToggle}>
+                                            {!searchToggle && (
+                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M20 20L16.2223 16.2156M18.3158 11.1579C18.3158 13.0563 17.5617 14.8769 16.2193 16.2193C14.8769 17.5617 13.0563 18.3158 11.1579 18.3158C9.2595 18.3158 7.43886 17.5617 6.0965 16.2193C4.75413 14.8769 4 13.0563 4 11.1579C4 9.2595 4.75413 7.43886 6.0965 6.0965C7.43886 4.75413 9.2595 4 11.1579 4C13.0563 4 14.8769 4.75413 16.2193 6.0965C17.5617 7.43886 18.3158 9.2595 18.3158 11.1579V11.1579Z" stroke="black" stroke-width="1.5" stroke-linecap="round"/>
+                                                </svg>
+                                            ) }
+                                            {searchToggle && (
+                                                <svg fill="#000000" width="24" height="22" viewBox="0 0 24 24" id="cross" data-name="Line Color" xmlns="http://www.w3.org/2000/svg" class="icon line-color cross-icon"><line id="primary" x1="19" y1="19" x2="5" y2="5" ></line><line id="primary-2" data-name="primary" x1="19" y1="5" x2="5" y2="19"></line></svg>
+                                            )}
+                                        </span>
                                     </li>
                                 </ul>
                             </div>
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import './Search.scss';
+import { Link } from "react-router-dom";
+
+const Search = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [isTyping, setIsTyping] = useState(false);
+  const [products, setProducts] = useState([])
+  const [ProductSuggestions, setProductSuggestions] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(data=>setProducts(data))
+  },[])
+
+  const handleChange = (e) => {
+    let value = e.target.value;
+    if (value != "") {
+      const filterProducts = products.filter(p => {
+        if(p.title.toLowerCase().includes(value.toLocaleLowerCase())) {
+            return p
+        }
+      }); 
+      setProductSuggestions(filterProducts);
+      setIsTyping(true);
+      setInputValue(value);
+    //   console.log(filterProducts)
+    } else {
+        setProductSuggestions(products);
+        setIsTyping(false);
+        setInputValue("");
+    }
+  };
+
+  const handleText = (e) => {
+    let clickedText = e.target.innerText;
+    setInputValue(clickedText);
+    setIsTyping(false);
+  };
+
+  return (
+    <div className="search">
+      <input
+        type="text"
+        onChange={handleChange}
+        value={inputValue}
+        placeholder="What are you looking for?."
+      />
+      <ul>
+        {isTyping &&
+          ProductSuggestions.map((product) => {
+            return (
+              <li onClick={handleText} key={product.id}>
+                <Link to={`/product/${product.id}`}>
+                    {product.title}
+                </Link>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+}
+
+export default Search;
